4da016667138507d3d006b9e724a6371bf8b851b
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-08-30 19:45:02 +0300

    updating daily routine

--- app/content/DailyRoutine/page.tsx
+++ app/content/DailyRoutine/page.tsx
@@ -10,9 +10,9 @@ const DailyRoutine = () => {
       </div>
 
       <ul>
-      <li>1 - Wake Uli at 7.50 A.M</li>
+      <li>1 - Wake Up at 7.50 A.M</li>
         <li>2 - Go To Work at 9.00 A.M</li>
-        <li>3 - Come Home At 6.00 li.M</li>
+        <li>3 - Come Home At 6.00 P.M</li>
         <li>4 - Shower</li>
         <li>5- Work on 2014 Resolution</li> 
         <li>6- Work on 2024 Resolution</li>
