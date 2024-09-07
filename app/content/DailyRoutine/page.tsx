ae40c0cfe7097669966aecf7a869bf786783f977
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-09-07 07:54:02 +0300

    2016 Resolution

--- app/content/DailyRoutine/page.tsx
+++ app/content/DailyRoutine/page.tsx
@@ -14,8 +14,10 @@ const DailyRoutine = () => {
         <li>2 - Go To Work at 9.00 A.M</li>
         <li>3 - Come Home At 6.00 P.M</li>
         <li>4 - Shower</li>
-        <li>5- Work on 2014 Resolution</li> 
         <li>6- Work on 2024 Resolution</li>
+        <li>5- Work on 2014 Resolution</li>
+        <li>6- Work on 2015 Resolution</li>
+        <li>6- Work on 2016 Resolution</li> 
       </ul>
     </>
   );
