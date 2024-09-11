69a4d51f8b668ebe76168a1fbeda2d1544f1e0a8
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-09-11 21:13:40 +0300

    apoolo updating

--- app/content/Apollo/page.tsx
+++ app/content/Apollo/page.tsx
@@ -5,7 +5,7 @@ const Apollo = () => {
     <>
       <main className="container justify-evenly w-full bg-blue-300">
         <iframe
-          className="p-12 bg-blue-300"
+          className="m-auto bg-blue-300"
           width="1600"
           height="900"
           src="https://www.eporner.com/embed/c2d8tOMqifu/"
