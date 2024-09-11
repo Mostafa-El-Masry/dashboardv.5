16c6f47bcc6aab7fe4d3f9bef11a4ef23ded1300
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-09-11 21:01:55 +0300

    apoolo updating

--- app/content/Apollo/page.tsx
+++ app/content/Apollo/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Apollo = () => {
   return (
     <>
-      <main className="flex">
+      <main className="container">
         <iframe
           className="p-12"
           width="1080"
