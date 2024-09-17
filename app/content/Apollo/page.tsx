577305afa553ce2eed6404fbc4fed0dcdf059280
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-09-17 22:06:11 +0300

    main page updateing

--- app/content/Apollo/page.tsx
+++ app/content/Apollo/page.tsx
@@ -6,135 +6,102 @@ const Apollo = () => {
       <main className="container justify-evenly w-full">
         <iframe
           className="py-2 mx-auto w-full aspect-video"
-          
           src="https://www.eporner.com/embed/c2d8tOMqifu/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/FdxTBZjCn2p/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/AvLi5e5Pa90/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/zodCASZYukW/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/BN00h9irndl/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/jXQtbVUr4DM/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/ecTlSotWKZZ/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/cnU5qir5mIz/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/8A7gz3I4Rwt/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/4OP7OA8SjUi/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/r4pkcu9wRcB/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/nx8mVIlSbLY/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/KOO4smnHAuI/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/VvptCiEy0a7/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/E1EHTRQ6iAh/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/OGC0N9sLq0F/"
           aria-controls="enable"
           allowFullScreen
         ></iframe>
         <iframe
-          className="py-2 mx-auto"
-          width="1500"
-          height="900"
+          className="py-2 mx-auto w-full aspect-video"
           src="https://www.eporner.com/embed/ZQRnBv8J4dD/" 
           aria-controls="enable"
           allowFullScreen
