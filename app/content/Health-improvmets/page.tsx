a0010751b228388f05050b7943f004c101f284e2
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-09-20 20:49:03 +0300

    updating h1 to .h1

--- app/content/Health-improvmets/page.tsx
+++ app/content/Health-improvmets/page.tsx
@@ -6,8 +6,8 @@ const Healthimprovmets = () => {
     <>
       <h1 className="h1">Health improvmets</h1>
       <Link className='Link' href="/">Home</Link>
-      <h1><Link href="/content/Health-improvmets/Mental">Mental Health</Link></h1>
-      <h1><Link href="/content/Health-improvmets/Pysical">Pysical Health</Link></h1>
+      <h1 className="h1"><Link href="/content/Health-improvmets/Mental">Mental Health</Link></h1>
+      <h1 className="h1"><Link href="/content/Health-improvmets/Pysical">Pysical Health</Link></h1>
     </>
   )
 }
