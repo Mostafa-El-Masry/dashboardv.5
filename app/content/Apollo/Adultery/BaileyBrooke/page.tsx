31e9d78a112b29cc7c2b88c8cbd792c8c4ab170b
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-09-20 13:29:24 +0300

    ADDING aDULTRY ADDING Bailey Brooke

---
+++ app/content/Apollo/Adultery/BaileyBrooke/page.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link'
+
+const BaileyBrooke = () => {
+  return (
+    <>
+    <h1>Bailey Brooke</h1>
+    <Link className='Link' href="/">Home</Link>
+        <iframe
+          className="py-2 mx-auto w-11/12 aspect-video"
+          
+          aria-controls="enable"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          className="py-2 mx-auto w-11/12 aspect-video"
+          
+          aria-controls="enable"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          className="py-2 mx-auto w-11/12 aspect-video"
+          
+          aria-controls="enable"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          className="py-2 mx-auto w-11/12 aspect-video"
+          
+          aria-controls="enable"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          className="py-2 mx-auto w-11/12 aspect-video"
+          
+          aria-controls="enable"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          className="py-2 mx-auto w-11/12 aspect-video"
+          
+          aria-controls="enable"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          className="py-2 mx-auto w-11/12 aspect-video"
+          
+          aria-controls="enable"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          className="py-2 mx-auto w-11/12 aspect-video"
+          
+          aria-controls="enable"
+          allowFullScreen
+        ></iframe>
+    </>
+    
+  )
+}
+
+export default BaileyBrooke
--- app/content/Apollo/page.tsx
+++ app/content/Apollo/page.tsx
@@ -1,114 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Apollo = () => {
   return (
     <>
       <main className="container justify-evenly w-full">
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/c2d8tOMqifu/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/FdxTBZjCn2p/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/AvLi5e5Pa90/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/zodCASZYukW/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/BN00h9irndl/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-      
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/ecTlSotWKZZ/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/cnU5qir5mIz/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/8A7gz3I4Rwt/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/4OP7OA8SjUi/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
+      <h1>Apollo</h1>
+      <Link className='Link' href="/">Home</Link>
+      <Link className='Link' href="/content/Apollo/Adultery">Adultery</Link>
 
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/nx8mVIlSbLY/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/KOO4smnHAuI/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/VvptCiEy0a7/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/E1EHTRQ6iAh/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/OGC0N9sLq0F/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/ZQRnBv8J4dD/" 
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/tKfAQNlOAuY/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          className="py-2 mx-auto w-10/12 aspect-video"
-          src="https://www.eporner.com/embed/P1zMHcvPuFb/"
-          aria-controls="enable"
-          allowFullScreen
-        ></iframe>
+        
       </main>
     </>
   );
