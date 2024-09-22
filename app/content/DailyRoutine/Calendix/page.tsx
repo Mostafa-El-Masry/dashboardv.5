3446ca87b60a10775b040ad80119ece3c8cdc957
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-09-22 19:17:23 +0300

    updating calendix

--- app/content/DailyRoutine/Calendix/page.tsx
+++ app/content/DailyRoutine/Calendix/page.tsx
@@ -24,12 +24,26 @@ const Calendix = () => {
         </Link>
         <Link
           href={"/content/DailyRoutine/Calendix"}
-          className="flex self-center gap-2 border rounded-full py-3 px-4"
+          className="flex self-center gap-2 border border-gray-300 rounded-full py-3 px-4"
         >
           <Play />
           Watch a Video
         </Link>
       </div>
+
+      <section className="text-center mt-24">
+        <p>Trusted By</p>
+        <div className="flex gap-12 *:h-8 mt-4 justify-center text-green-600" >
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/7dsuPwH4V8KJvCexSZueZc/272b2ef619de8ae4b443758413a19733/Unbounce_Logo.svg" alt=""></img>
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/1FA2gEsWeu2MyGSy6Qp6ao/859833105cdd6ed5cc75eb5e4bd9cff7/HelloFresh.svg" alt=""></img>
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/4Y87kRrhSPSYgUbSWYxP1z/a13177cf43f99e7a79c691c54e271a98/Hubspot.svg" alt=""></img>
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/vHHaKAaEeQuNcucdWM50V/23351da3b1ad9d7483ddf11aed64b4b7/Mixpanel.svg" alt=""></img>
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/7dsuPwH4V8KJvCexSZueZc/272b2ef619de8ae4b443758413a19733/Unbounce_Logo.svg" alt=""></img>
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/1FA2gEsWeu2MyGSy6Qp6ao/859833105cdd6ed5cc75eb5e4bd9cff7/HelloFresh.svg" alt=""></img>
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/4Y87kRrhSPSYgUbSWYxP1z/a13177cf43f99e7a79c691c54e271a98/Hubspot.svg" alt=""></img>
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/vHHaKAaEeQuNcucdWM50V/23351da3b1ad9d7483ddf11aed64b4b7/Mixpanel.svg" alt=""></img>
+        </div>
+      </section>
     </main>
   );
 };
